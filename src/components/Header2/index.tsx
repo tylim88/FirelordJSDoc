0199457f4f09c00d5699978fe10e403799cf6e44
tylim <limteikyee@gmail.com>
2022-07-23 14:21:42 +0800

    update

--- src/components/Header2/index.tsx
+++ src/components/Header2/index.tsx
@@ -2,38 +2,38 @@ import React from 'react'
 
 export const Header = () => (
 	<>
-		<br />
 		<div align="center">
 			<img
 				src="https://raw.githubusercontent.com/tylim88/Firelord/main/img/ozai.png"
 				width="200px"
 			/>
-			<h1>FirelordJS 烈火君JS</h1>
+			<h1>Firelord 烈火君</h1>
 		</div>
+
 		<div align="center">
-			<a href="https://www.npmjs.com/package/firelordjs" target="_blank">
+			<a href="https://www.npmjs.com/package/firelord" target="_blank">
 				<img
-					src="https://img.shields.io/npm/v/firelordjs"
+					src="https://img.shields.io/npm/v/firelord"
 					alt="Created by tylim88"
 				/>
 			</a>
 			&nbsp;
 			<a
-				href="https://github.com/tylim88/firelordjs/blob/main/LICENSE"
+				href="https://github.com/tylim88/firelord/blob/main/LICENSE"
 				target="_blank"
 			>
 				<img
-					src="https://img.shields.io/github/license/tylim88/firelordjs"
+					src="https://img.shields.io/github/license/tylim88/firelord"
 					alt="License"
 				/>
 			</a>
 			&nbsp;
 			<a
-				href="https://www.npmjs.com/package/firelordjs?activeTab=dependencies"
+				href="https://www.npmjs.com/package/firelord?activeTab=dependencies"
 				target="_blank"
 			>
 				<img
-					src="https://img.shields.io/badge/dynamic/json?url=https://api.npmutil.com/package/firelordjs&label=dependencies&query=$.dependencies.count&color=brightgreen"
+					src="https://img.shields.io/badge/dynamic/json?url=https://api.npmutil.com/package/firelord&label=dependencies&query=$.dependencies.count&color=brightgreen"
 					alt="dependency count"
 				/>
 			</a>
@@ -43,24 +43,24 @@ export const Header = () => (
 				alt="package size"
 			/>
 			&nbsp;
-			<a href="https://github.com/tylim88/Firelordjs/actions" target="_blank">
+			<a href="https://github.com/tylim88/Firelord/actions" target="_blank">
 				<img
-					src="https://github.com/tylim88/Firelordjs/workflows/Main/badge.svg"
+					src="https://github.com/tylim88/Firelord/workflows/Main/badge.svg"
 					alt="github action"
 				/>
 			</a>
 			&nbsp;
-			<a href="https://codecov.io/gh/tylim88/Firelordjs" target="_blank">
+			<a href="https://codecov.io/gh/tylim88/Firelord" target="_blank">
 				<img
-					src="https://codecov.io/gh/tylim88/Firelordjs/branch/main/graph/badge.svg"
+					src="https://codecov.io/gh/tylim88/Firelord/branch/main/graph/badge.svg"
 					alt="code coverage"
 				/>
 			</a>
 			&nbsp;
-			<a href="https://github.com/tylim88/Firelordjs/issues" target="_blank">
+			<a href="https://github.com/tylim88/Firelord/issues" target="_blank">
 				<img
 					alt="GitHub issues"
-					src="https://img.shields.io/github/issues-raw/tylim88/firelordjs"
+					src="https://img.shields.io/github/issues-raw/tylim88/firelord"
 				></img>
 			</a>
 			&nbsp;
@@ -72,30 +72,30 @@ export const Header = () => (
 			</a>
 			&nbsp;
 			<a
-				href="https://lgtm.com/projects/g/tylim88/Firelordjs/alerts/"
+				href="https://lgtm.com/projects/g/tylim88/Firelord/alerts/"
 				target="_blank"
 			>
 				<img
 					alt="Total alerts"
-					src="https://img.shields.io/lgtm/alerts/g/tylim88/Firelordjs.svg?logo=lgtm&logoWidth=18"
+					src="https://img.shields.io/lgtm/alerts/g/tylim88/Firelord.svg?logo=lgtm&logoWidth=18"
 				/>
 			</a>
 			&nbsp;
 			<a
-				href="https://lgtm.com/projects/g/tylim88/Firelordjs/context:javascript"
+				href="https://lgtm.com/projects/g/tylim88/Firelord/context:javascript"
 				target="_blank"
 			>
 				<img
 					alt="Language grade: JavaScript"
-					src="https://img.shields.io/lgtm/grade/javascript/g/tylim88/Firelordjs.svg?logo=lgtm&logoWidth=18"
+					src="https://img.shields.io/lgtm/grade/javascript/g/tylim88/Firelord.svg?logo=lgtm&logoWidth=18"
 				/>
 			</a>
 		</div>
 		<br />
 		<div align="center">
 			<i>
-				Extremely High Precision Typescript Wrapper for Firestore Web, Providing
-				Unparalleled Type Safe and Dev Experience
+				Extremely High Precision Typescript Wrapper for Firestore Admin,
+				Providing Unparalleled Type Safe and Dev Experience
 			</i>
 		</div>
 		<br />
@@ -104,6 +104,17 @@ export const Header = () => (
 				Modular, Minuscule, Intuitive, Unopinionated, Craftsmanship, Ultimate,
 				Peaceful, Deep
 			</i>
+		</div>
+		<br />
+		<div align="center">
+			<i>
+				Of The VFQAT &#160;&#160;||&#160;&#160; By The VFQAT
+				&#160;&#160;||&#160;&#160; For The VFQAT
+			</i>
+		</div>
+		<br />
+		<div align="center">
+			<i>End Firestore Typing Madness</i>
 		</div>
 		<br />
 	</>
